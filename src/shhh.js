cd9880cb43ca61a5f42aff401e493c0a2e8e8954
Darr <118402014+DigitalDarr@users.noreply.github.com>
2025-03-01 12:35:18 -0500

    Update shhh.js

--- src/shhh.js
+++ src/shhh.js
@@ -1,4 +1,4 @@
 // shhh.js
-export const PROJECT_ID = "67afce5500283e806cc0";
-export const DATABASE_ID = "67afcf0b002418982406"
-export const COLLECTION_ID = "67afcf52003e2a4a26d3"
+export const PROJECT_ID = " ";
+export const DATABASE_ID = " "
+export const COLLECTION_ID = " "
